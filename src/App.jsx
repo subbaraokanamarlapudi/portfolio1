d21c88c2ec4b7192386209635a101ce3c7b5641a
KANAMARLAPUDI VENKATA SUBBARAO <subbaraokvsr890@gmail.com>
2025-12-03 10:43:01 +0530

    experience and project are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Projects from "./pages/Projects";
+import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-// import Experience from "./pages/Experience";
+import Experience from "./pages/Experience";
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/experience" element={<Experience />} />
-          <Route path="/projects" element={<Projects />} /> */}
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
